a27e0126191c65d889fa9620e679abb348c179ca
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-05-09 21:55:18 +0300

    hw initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,94 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import Notification from './Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const statIncrease = () => {
+    setGood(prev => prev + 1);
   };
 
-  statIncrease = ({ target }) => {
-    const { name } = target;
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const { good } = this.state;
-    return Math.round((good / total) * 100);
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Section title="Please leave your feedback">
-          <FeedbackOptions
-            options={['Good', 'Neutral', 'Bad']}
-            onFeedback={this.statIncrease}
+  return (
+    <>
+      <Section title="Please leave your feedback">
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onFeedback={statIncrease}
+        />
+      </Section>
+      {/* <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percentage={percentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              percentage={percentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section> */}
+    </>
+  );
 }
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   statIncrease = ({ target }) => {
+//     const { name } = target;
+//     this.setState(prevState => {
+//       return { [name]: prevState[name] + 1 };
+//     });
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     const total = this.countTotalFeedback();
+//     const { good } = this.state;
+//     return Math.round((good / total) * 100);
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = this.countTotalFeedback();
+//     const percentage = this.countPositiveFeedbackPercentage();
+//     return (
+//       <>
+//         <Section title="Please leave your feedback">
+//           <FeedbackOptions
+//             options={['Good', 'Neutral', 'Bad']}
+//             onFeedback={this.statIncrease}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {total > 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               percentage={percentage}
+//             />
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//       </>
+//     );
+//   }
+// }
